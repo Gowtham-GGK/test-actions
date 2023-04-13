57f2af8d151e171b2cc98f28eb9ca87c0f07b307
Gowtham <gowthamaaju@gmail.com>
2023-04-13 13:44:55 +0000

    creating files

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-''' Test Script '''
 import os
 
-with open('./test/sample.txt', encoding="utf-8") as f:
-    print(f.read())
-    os.mkdir('./create/')
+# Define the file path and contents
+file_path = 'path/to/new_file.txt'
+file_contents = 'This is the contents of the new file.'
+
+# Create the file
+with open(file_path, 'w') as f:
+    f.write(file_contents)
+
+# Commit the file to the repository
+os.system('git add {} && git commit -m "Added new file" && git push'.format(file_path))
